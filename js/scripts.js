a0b10f69925194359dd63d8e0aa9f0c8715bb0fc
VadimSokolovsky <Sokolovsky.v.a@gmail.com>
2021-05-08 20:02:40 +0300

    feat: add form

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,25 @@ jQuery(document).ready(function($) {
             $('.main-header-wrapper').removeClass('active');
         }
     });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+        } else {
+        $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+    $("#form-send").submit(function (e) {
+        e.preventDefault();
+        $.post("mailsender.php", $(this).serialize(), function (data) {
+            if (data == 'error_tel') {
+                $("#form-send").append("<p class='error__number'>Неправильно введен номер</p>");
+            } else {
+                $("#form-send").html("<p>Спасибо, Вам перезвонят в ближайшее время</p>");
+            }
+        });
+    });
 });
